fe7ef46a27c4e50ad3bd19e0fada16be28f4233c
Shree716 <122548030+Shree716@users.noreply.github.com>
2023-01-19 11:25:27 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,19 @@
 input.onButtonPressed(Button.A, function () {
     music.playMelody("C5 B A G F E D C ", 120)
 })
+input.onSound(DetectedSound.Loud, function () {
+    basic.showString("Beta!! How Dare You I Am God")
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showIcon(IconNames.Heart)
+})
 input.onButtonPressed(Button.B, function () {
-    basic.showString("sup im the goat")
+    basic.showString("Sup I am The Goat")
+    basic.showString("your not")
 })
 input.onGesture(Gesture.Shake, function () {
     basic.showIcon(IconNames.Surprised)
 })
-basic.forever(function () {
-	
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    music.playMelody("C5 G B A F A C5 B ", 120)
 })
